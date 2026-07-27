2a110b433e85e3bfa3bc4822b2b1caf5d553d1cb
sajjad <sajjadzolghadr777@gmail.com>
2026-07-27 13:07:05 +0330

    fix:change login view and redirect url for login and logout

--- socialproject/users/urls.py
+++ socialproject/users/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('',views.index,name='index'),
-    path('login/',views.user_login,name='login'),
+    path('login/',auth_views.LoginView.as_view(template_name ='users/login.html'),name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name ='users/logout.html' ),name='logout'),
     path('password_change/',auth_views.PasswordChangeView.as_view(template_name ='users/password_change.html'),name='password_change'),
     path('password_change_done/',auth_views.PasswordChangeDoneView.as_view(template_name ='users/password_change_done.html'),name='password_change_done'),
--- socialproject/users/views.py
+++ socialproject/users/views.py
@@ -1,24 +1,6 @@
 from django.shortcuts import render
-from .forms import LoginForm
-from django.http import HttpResponse
-from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
 # Create your views here.
-def user_login(request):
-    if request.method == "POST":
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            data = form.cleaned_data
-            user = authenticate(username=data['username'], password=data['password'])
-            if user is not None:
-                login(request, user)
-                return HttpResponse("Login Successful")
-            else:
-                return HttpResponse("Login Failed")
-    else:
-        form = LoginForm()
-    return render(request, 'users/login.html', {'form': form})
-
 @login_required(login_url='login')
 def index(request):
     return render(request, 'users/index.html')
