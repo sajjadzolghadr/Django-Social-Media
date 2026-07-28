ffd76bfc35100a8ea27e29dd4654df0fa88b891d
sajjad <sajjadzolghadr777@gmail.com>
2026-07-28 12:53:42 +0330

    feat: add password reset flow and improve login template

--- socialproject/users/urls.py
+++ socialproject/users/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('logout/',auth_views.LogoutView.as_view(template_name ='users/logout.html' ),name='logout'),
     path('password_change/',auth_views.PasswordChangeView.as_view(template_name ='users/password_change.html'),name='password_change'),
     path('password_change_done/',auth_views.PasswordChangeDoneView.as_view(template_name ='users/password_change_done.html'),name='password_change_done'),
-
+    path('password_reset',auth_views.PasswordResetView.as_view(template_name ='users/password_reset.html'),name='password_reset'),
+    path('password_reset_done',auth_views.PasswordResetDoneView.as_view(template_name ='users/password_reset_done.html'),name='password_reset_done'),
+    path("reset/<uidb64>/<token>/",auth_views.PasswordResetConfirmView.as_view(template_name="users/password_reset_confirm.html"),name="password_reset_confirm"),
+    path("reset/complete/",auth_views.PasswordResetCompleteView.as_view(template_name="users/password_reset_complete.html"),name="password_reset_complete")
 ]
