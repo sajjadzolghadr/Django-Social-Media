d15272aa54e4300206fdd92b861530ca9b8f1f35
sajjad <sajjadzolghadr777@gmail.com>
2026-07-29 19:12:09 +0330

    feat: implement user registration and update login page

--- socialproject/users/views.py
+++ socialproject/users/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .froms import UserRegistrationForm
 # Create your views here.
 @login_required(login_url='login')
 def index(request):
     return render(request, 'users/index.html')
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return render(request, 'users/register_done.html')
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'users/register.html', {'user_form': user_form})
