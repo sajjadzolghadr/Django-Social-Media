145400d06a00f526ac6e818f3437172ba263fe3e
sajjad <sajjadzolghadr777@gmail.com>
2026-07-30 10:56:20 +0330

    feat: implement user and profile edit forms

--- socialproject/users/froms.py
+++ socialproject/users/froms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-
+from .models import Profile
 
 class UserRegistrationForm(forms.ModelForm):
     password = forms.CharField(label='Password',widget=forms.PasswordInput)
@@ -12,3 +12,13 @@ class UserRegistrationForm(forms.ModelForm):
         if self.cleaned_data['password'] != self.cleaned_data['password2']:
             raise forms.ValidationError('Passwords must match')
         return self.cleaned_data['password']
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['photo']
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username','email','first_name']
--- socialproject/users/urls.py
+++ socialproject/users/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path("reset/<uidb64>/<token>/",auth_views.PasswordResetConfirmView.as_view(template_name="users/password_reset_confirm.html"),name="password_reset_confirm"),
     path("reset/complete/",auth_views.PasswordResetCompleteView.as_view(template_name="users/password_reset_complete.html"),name="password_reset_complete"),
     path('register/',views.register,name='register'),
+    path('edit/',views.edit_profile,name='edit'),
 ]
