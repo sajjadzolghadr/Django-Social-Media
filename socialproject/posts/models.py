b67a81c335ef1194c7ceb864aede60e9a2e8caac
sajjad <sajjadzolghadr777@gmail.com>
2026-07-30 11:11:37 +0330

    feat: add posts app and create post model

---
+++ socialproject/posts/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.conf import settings
+# Create your models here.
+class Post(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/%Y/%m/%d')
+    caption = models.TextField(blank=True)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.title
+
+
