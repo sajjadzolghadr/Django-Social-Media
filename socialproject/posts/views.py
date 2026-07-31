ffbeb1edf2dec75b5d535fc4bab2aca63702f76b
sajjad <sajjadzolghadr777@gmail.com>
2026-07-31 13:46:10 +0330

    feat: add post creation template, configure URLs, and update index view

--- socialproject/posts/views.py
+++ socialproject/posts/views.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from .forms import PostForm
 # Create your views here.
+@login_required(login_url='login')
 def post_create(request):
     if request.method == 'POST':
-        form = PostForm(data=request.POST)
+        form = PostForm(data=request.POST,files=request.FILES)
         if form.is_valid():
             post = form.save(commit=False)
             post.user = request.user
